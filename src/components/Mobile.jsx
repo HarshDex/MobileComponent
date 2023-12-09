4c4fed9b3d02cd39658f4809a28f9696dd2543c3
HarshDex <nature4917@gmail.com>
2023-12-09 11:17:04 +0530

    added rafce

---
+++ src/components/Mobile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Mobile = () => {
+  return (
+    <div className="mobile-container">
+        <div className="mobile-notch"></div>
+    </div>
+  )
+}
+
+export default Mobile
